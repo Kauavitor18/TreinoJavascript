980af86fb9e31a0ddb97bcc5b4528e150f02cb3c
Kauavitor18 <118356349+Kauavitor18@users.noreply.github.com>
2024-06-07 23:05:29 -0300

    adicionando arquivo css

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ idade = prompt("Digite sua idade: ");
 numero = prompt("Digite seu numero: ");
 
 
-document.getElementById("bem-vindo").innerHTML = "Seja bem vindo"+" "+ nome;
-document.getElementById("confirmacao").innerHTML = "Sua idade é:" + " "+  idade + "e seu número é: " + numero;
+document.getElementById("bem-vindo").innerHTML = "Seja bem vindo" + " " + nome;
+document.getElementById("confirmacao").innerHTML = "Sua idade é" + " "+  idade + " e seu número é " + numero;
